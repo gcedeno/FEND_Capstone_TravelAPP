be7d65b2b0739a225accfa86345b741752b60bf5
GustavoCedeno <gustavo.a.cedeno@gmail.com>
2020-04-14 20:36:08 +0200

    adding basic index.js functionality

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,8 @@
-import { validURL } from './js/urlChecker'
+// import main app functions
+import { somefunction } from './js/app.js'
 import { handleSubmit } from './js/formHandler'
+
+//import styles
 import './styles/resets.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
@@ -7,9 +10,16 @@ import './styles/base.scss'
 import './styles/header.scss'
 import './styles/messages.scss'
 
+//importing pixabayImage, require when using their api
+import pixabayImage from './media/pixabay-logo.png'
+
+//Calling and using main functions imported from the js scripts
+somefunction()
+handleSubmit()
+
 // export as module
 export{
-	validURL,
+	somefunction,
 	handleSubmit
 }
  
