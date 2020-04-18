77134aa3dd2c046fe985143fac8f78458f337e28
GustavoCedeno <gustavo.a.cedeno@gmail.com>
2020-04-18 11:34:58 +0200

    New scripts for API interaction

---
+++ src/client/js/geonames.js
@@ -0,0 +1,5 @@
+// Script for fetching data using the geonames API
+// Gettng the API credentials from the .env file
+require('dotenv').config()
+//required fetching functionality 
+import fetch from 'node-fetch';
