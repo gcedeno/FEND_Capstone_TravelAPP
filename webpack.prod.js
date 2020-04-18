80809987de088c0eb73036dadd1f275469f9eb61
GustavoCedeno <gustavo.a.cedeno@gmail.com>
2020-04-18 19:48:33 +0200

    new config for webpack.prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,94 +1,34 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const TerserPlugin = require('terser-webpack-plugin');
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+const merge = require('webpack-merge')
+const TerserJSPlugin = require('terser-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
+const common = require('./webpack.common')
 
-module.exports = {
-    entry: './src/client/index.js',
-    output:{
-        libraryTarget: 'var',
-        library: 'Client',
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.min.js'
-	   },
-    mode: 'production',
-    devtool: 'source-map',
-    
-    optimization: {
-        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
-    },
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.s?[ac]ss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-              },
-              {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d\.\d+\.\d+)?$/,
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {
-                      name: '[name].[ext]',
-                    },
-                  },
-                ],
-              },
-            {
-            test: /\.s?[ac]ss$/,
-            use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
-            },
-            {
-            test: /\.(png|jpe?g|gif)$/i,
-            use: [
-            {
-                loader: 'file-loader',
-                options: {
-                name(file) {
-                    if (process.env.NODE_ENV === 'development') {
-                    return '[path][name].[ext]'
-                    }
-                    return '[contenthash].[ext]'
-                },
-                },
-            },
-            ],
-        }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new WorkboxPlugin.GenerateSW({
-            clientsClaim: true,
-            skipWaiting: true,
-            navigateFallback:'index.html',
-            cleanupOutdatedCaches: true,
-        }),
-        new MiniCssExtractPlugin({
-            filename:'[name].css',
-            chunkFilename:'[id].css',
-        }),
-        new HtmlWebPackPlugin({
-            title:'Travel App',
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
     ],
-    resolve: {
-        extensions: ['.js'],
-      },
-      output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, '../dist'),
-      },
-
-}
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+      navigateFallback: 'index.html',
+      cleanupOutdatedCaches: true,
+    }),
+  ],
+})
