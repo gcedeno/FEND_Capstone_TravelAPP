b307eb7d6441c3baf0d3c7690484d74de87beff5
GustavoCedeno <gustavo.a.cedeno@gmail.com>
2020-04-18 20:06:07 +0200

    basic webpack config

--- src/client/index.js
+++ src/client/index.js
@@ -22,62 +22,62 @@ showDestinations()
 const pixabay = document.getElementById('pixabay')
 pixabay.setAttribute('src', pixabayImage)
 
-// Checks if serviceworker is available
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-  })
-}
+// // Checks if serviceworker is available
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js')
+//   })
+// }
 
-// Initial check if offline
-if (!navigator.onLine) {
-	showOfflineMessage()
-  }
+// // Initial check if offline
+// if (!navigator.onLine) {
+// 	showOfflineMessage()
+//   }
   
-  // Event listener for being online again
-  window.addEventListener('online', () => {
-	const elements = retrieveInputs()
-	elements.forEach(el => {
-	  if (el.hasAttribute('id')) {
-		el.removeAttribute('disabled')
-	  }
-	})
-	removeOfflineMessage()
-  })
+//   // Event listener for being online again
+//   window.addEventListener('online', () => {
+// 	const elements = retrieveInputs()
+// 	elements.forEach(el => {
+// 	  if (el.hasAttribute('id')) {
+// 		el.removeAttribute('disabled')
+// 	  }
+// 	})
+// 	removeOfflineMessage()
+//   })
   
-  // Event listener for being offline
-  window.addEventListener('offline', () => {
-	const elements = retrieveInputs()
-	elements.forEach(el => {
-	  el.setAttribute('disabled', 'disabled')
-	})
-	showOfflineMessage()
-  })
+//   // Event listener for being offline
+//   window.addEventListener('offline', () => {
+// 	const elements = retrieveInputs()
+// 	elements.forEach(el => {
+// 	  el.setAttribute('disabled', 'disabled')
+// 	})
+// 	showOfflineMessage()
+//   })
   
-  // removes the message when user turn online again
-  const removeOfflineMessage = () => {
-	const offline = document.getElementById('offline')
-	if (offline) {
-	  offline.remove()
-	}
-  }
+//   // removes the message when user turn online again
+//   const removeOfflineMessage = () => {
+// 	const offline = document.getElementById('offline')
+// 	if (offline) {
+// 	  offline.remove()
+// 	}
+//   }
   
-  // shows an message at the top of the page that the user turned offline
-  const showOfflineMessage = () => {
-	const first = document.querySelector('main')
-	if (document.getElementById('offline')) {
-	  return
-	}
-	const offline = "<p id='offline'>You seem to be offline!</p>"
-	first.insertAdjacentHTML('afterbegin', offline)
-  }
+//   // shows an message at the top of the page that the user turned offline
+//   const showOfflineMessage = () => {
+// 	const first = document.querySelector('main')
+// 	if (document.getElementById('offline')) {
+// 	  return
+// 	}
+// 	const offline = "<p id='offline'>You seem to be offline!</p>"
+// 	first.insertAdjacentHTML('afterbegin', offline)
+//   }
   
-  // Disable all inputs and buttons
-  const retrieveInputs = () => {
-	const buttons = document.querySelectorAll('button')
-	const inputs = document.querySelectorAll('input')
-	return [...inputs, ...buttons]
-  }
+//   // Disable all inputs and buttons
+//   const retrieveInputs = () => {
+// 	const buttons = document.querySelectorAll('button')
+// 	const inputs = document.querySelectorAll('input')
+// 	return [...inputs, ...buttons]
+//   }
 // export as module
 export{
 	showNewSearch,
