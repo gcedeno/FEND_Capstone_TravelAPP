abdb19b56dd9c80cd9423ae6a26c25f7c14005a6
GustavoCedeno <gustavo.a.cedeno@gmail.com>
2020-04-18 13:18:12 +0200

    fetching data from pixabay

--- src/client/js/pixabay.js
+++ src/client/js/pixabay.js
@@ -3,3 +3,20 @@
 require('dotenv').config()
 //required fetching functionality 
 import fetch from 'node-fetch';
+
+// PixaBayApi to fetch a city image
+module.exports.fetchImage = async city => {
+    const {PIXABAY_BASEURL, PIXABAY_KEY} = process.env
+    try {
+      const result = await fetch(
+        `${PIXABAY_BASEURL}?q=${encodeURI(
+          city,
+        )}&image_type=photo&category=travel&key=${PIXABAY_KEY}`,
+      )
+      const {hits} = await result.json()
+      return (hits.length > 0 && hits[0].webformatURL) || ''
+    } catch (e) {
+      throw e
+    }
+  }
+  
